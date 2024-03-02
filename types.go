a7ba99d9af156b863de200c05baafe5559c028ae
gram <git@orsinium.dev>
2024-03-02 19:12:20 +0100

    implement int types

--- types.go
+++ types.go
@@ -2,11 +2,6 @@ package wypes
 
 type Int8 int8
 
-var (
-	_ Lift[Int8]  = Int8(0)
-	_ Lower[Int8] = Int8(0)
-)
-
 func (Int8) ValueTypes() []ValueType {
 	return []ValueType{ValueTypeI32}
 }
@@ -16,5 +11,61 @@ func (Int8) Lift(s Store) Int8 {
 }
 
 func (v Int8) Lower(s Store) {
+	s.Stack.Push(Raw(v))
+}
+
+type Int16 int16
+
+func (Int16) ValueTypes() []ValueType {
+	return []ValueType{ValueTypeI32}
+}
+
+func (Int16) Lift(s Store) Int16 {
+	return Int16(s.Stack.Pop())
+}
+
+func (v Int16) Lower(s Store) {
+	s.Stack.Push(Raw(v))
+}
+
+type Int32 int32
+
+func (Int32) ValueTypes() []ValueType {
+	return []ValueType{ValueTypeI32}
+}
+
+func (Int32) Lift(s Store) Int32 {
+	return Int32(s.Stack.Pop())
+}
+
+func (v Int32) Lower(s Store) {
+	s.Stack.Push(Raw(v))
+}
+
+type Int64 int64
+
+func (Int64) ValueTypes() []ValueType {
+	return []ValueType{ValueTypeI64}
+}
+
+func (Int64) Lift(s Store) Int64 {
+	return Int64(s.Stack.Pop())
+}
+
+func (v Int64) Lower(s Store) {
+	s.Stack.Push(Raw(v))
+}
+
+type Int int
+
+func (Int) ValueTypes() []ValueType {
+	return []ValueType{ValueTypeI64}
+}
+
+func (Int) Lift(s Store) Int {
+	return Int(s.Stack.Pop())
+}
+
+func (v Int) Lower(s Store) {
 	s.Stack.Push(Raw(v))
 }
--- types_test.go
+++ types_test.go
@@ -31,7 +31,12 @@ func (s *stack) Size() int {
 	return len(s.raw)
 }
 
-func TestInt8(t *testing.T) {
+type tripping[T any] interface {
+	wypes.Lower[T]
+	wypes.Lift[T]
+}
+
+func testRoundtrip[T tripping[T]](t *testing.T) {
 	c := is.NewRelaxed(t)
 	stack := newStack()
 	store := wypes.Store{Stack: stack}
@@ -41,7 +46,7 @@ func TestInt8(t *testing.T) {
 	is.Equal(c, stack.Size(), 1)
 
 	// lift, stack should be empty
-	var i wypes.Int8
+	var i T
 	i = i.Lift(store)
 	is.Equal(c, stack.Size(), 0)
 
@@ -52,4 +57,20 @@ func TestInt8(t *testing.T) {
 	// pop from the stack and check the value
 	is.Equal(c, stack.Pop(), 123)
 	is.Equal(c, stack.Size(), 0)
+}
+
+// Test that lifting and then lowering a value doesn't change the value.
+func TestRoundtrip(t *testing.T) {
+	t.Run("Int8", testRoundtrip[wypes.Int8])
+	t.Run("Int16", testRoundtrip[wypes.Int16])
+	t.Run("Int32", testRoundtrip[wypes.Int32])
+	t.Run("Int64", testRoundtrip[wypes.Int64])
+}
+
+// A static check that all primitive types can be implicitly cast from literals.
+func TestAssignLiteral(t *testing.T) {
+	var _ wypes.Int8 = 123
+	var _ wypes.Int16 = 12377
+	var _ wypes.Int32 = 1237777777
+	var _ wypes.Int64 = 1237777777777777777
 }
