5fb34ace91a1d7830f4b724e0de59254d9286c87
gram <git@orsinium.dev>
2024-03-03 09:36:12 +0100

    Complex64, Complex128

--- types_misc.go
+++ types_misc.go
@@ -55,6 +55,48 @@ func (v Float64) Lower(s Store) {
 	s.Stack.Push(Raw(r))
 }
 
+type Complex64 complex64
+
+func (Complex64) ValueTypes() []ValueType {
+	return []ValueType{ValueTypeF32, ValueTypeF32}
+}
+
+func (Complex64) Lift(s Store) Complex64 {
+	c := complex(
+		math.Float32frombits(uint32(s.Stack.Pop())),
+		math.Float32frombits(uint32(s.Stack.Pop())),
+	)
+	return Complex64(c)
+}
+
+func (v Complex64) Lower(s Store) {
+	r := math.Float32bits(real(v))
+	i := math.Float32bits(imag(v))
+	s.Stack.Push(Raw(r))
+	s.Stack.Push(Raw(i))
+}
+
+type Complex128 complex128
+
+func (Complex128) ValueTypes() []ValueType {
+	return []ValueType{ValueTypeF64, ValueTypeF64}
+}
+
+func (Complex128) Lift(s Store) Complex128 {
+	c := complex(
+		math.Float64frombits(uint64(s.Stack.Pop())),
+		math.Float64frombits(uint64(s.Stack.Pop())),
+	)
+	return Complex128(c)
+}
+
+func (v Complex128) Lower(s Store) {
+	r := math.Float64bits(real(v))
+	i := math.Float64bits(imag(v))
+	s.Stack.Push(Raw(r))
+	s.Stack.Push(Raw(i))
+}
+
 type Duration time.Duration
 
 func (Duration) ValueTypes() []ValueType {
